0c1c39cd21af47a8615ceda13a2548e8391a2484
Плотников Александр <aigoplotnikov@sberbank.ru>
2022-12-29 13:42:00 +0300

    handle error when file load

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,14 +1,12 @@
 import Typography from '@mui/material/Typography';
 import { default as MUIToolbar } from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
-import {useState} from 'react';
 
 export default function Toolbar({
     handleOpen,
     open,
-    setFrontApi
+    // setFrontApi
 }: any) {
     // const [isDemoDataButtonVisible, toggleDemoDataButtonVisible] = useState(true);
     // const loadDemoData = () => {
--- src/pages/graph/Graph.tsx
+++ src/pages/graph/Graph.tsx
@@ -12,12 +12,14 @@ import {GraphProvider} from './contexts/GraphProvider';
 import {parse} from 'yaml'
 import {BasicModal} from '../../components/BasicModal';
 import {VirtualizedList} from '../../components/VirtualizedList';
+import Typography from '@mui/material/Typography';
 
 export default function Graph() {
 	const [frontApi, setFrontApi] = useState<any>(null);
 	const [graphData, setGraphData] = useState<any>(null);
 	const [dependencyListMap, setDependencyListMap] = useState<any>(null);
 	const [fullGraphData, setFullGraphData] = useState(null);
+	const [hasError, setError] = useState(false);
 
 	const [opened, setOpenFileListModal] = useState(false);
 	const handleOpen = () => setOpenFileListModal(true);
@@ -45,13 +47,18 @@ export default function Graph() {
 	useEffect(() => {
 		if (!frontApi) return;
 
-		const hierarchyGraphData = generateSchema(frontApi);
-		const graphSchema = new GraphSchema();
-		const dependencyListMap = graphSchema.generateDependencyList(hierarchyGraphData);
-
-		setDependencyListMap(dependencyListMap);
-		setFullGraphData(hierarchyGraphData);
-		setGraphData(hierarchyGraphData);
+		try {
+			const hierarchyGraphData = generateSchema(frontApi);
+			const graphSchema = new GraphSchema();
+			const dependencyListMap = graphSchema.generateDependencyList(hierarchyGraphData);
+	
+			setDependencyListMap(dependencyListMap);
+			setFullGraphData(hierarchyGraphData);
+			setGraphData(hierarchyGraphData);
+			setError(false);
+		} catch (error) {
+			setError(true);
+		}
 	}, [frontApi]);
 
 	useEffect(() => {
@@ -66,6 +73,7 @@ export default function Graph() {
 			const yamlToJson = parse(yamlFile);
 
 			setFrontApi(yamlToJson)
+			setError(true);
 
 			return;
 		}
@@ -105,6 +113,17 @@ export default function Graph() {
 					fullGraphData={fullGraphData}
 				/>
 			</Sidebar>
+			{
+				hasError &&
+				<div style={{textAlign: 'center', marginTop: 50}}>
+					<Typography variant='h5' noWrap sx={{ flexGrow: 1 }} component='div'>
+						Произошла ошибка при обработке загруженного файла.
+					</Typography>
+					<Typography variant='h5' noWrap sx={{ flexGrow: 1 }} component='div'>
+						Проверьте, что вы загружаете файл формата .yml или .yaml, а также, что он соответствует стандарту OpenAPI 3.0
+					</Typography>
+				</div>
+			}
 			<HierarchyGraphContainer
 				graphWidth={isDrawerOpen ? window.innerWidth - SIDEBAR_WIDTH + 55 : window.innerWidth} // 55 - дополнительный отступ
 				data={graphData}
